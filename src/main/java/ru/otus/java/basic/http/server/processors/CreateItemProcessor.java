cc1c2a8276700b1cdb1fd4e3f02f4382f168b93e
idanilchenko@altsoft.biz <idanilchenko@altsoft.biz>
2025-05-04 11:37:40 +0300

    add check item

--- src/main/java/ru/otus/java/basic/http/server/processors/CreateItemProcessor.java
+++ src/main/java/ru/otus/java/basic/http/server/processors/CreateItemProcessor.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 import ru.otus.java.basic.http.server.HttpRequest;
 import ru.otus.java.basic.http.server.application.Item;
 import ru.otus.java.basic.http.server.application.ItemsRepository;
+import ru.otus.java.basic.http.server.exceptions.BadRequestException;
 
 import java.io.IOException;
 import java.io.OutputStream;
@@ -16,10 +17,20 @@ public class CreateItemProcessor implements RequestProcessor {
         this.itemsRepository = itemsRepository;
     }
 
+    private void checkItem(Item item) {
+        if (item.getTitle().isBlank()) {
+            throw new BadRequestException("400", "item title is blank");
+        }
+        if (item.getPrice().doubleValue() < 0) {
+            throw new BadRequestException("400", "item price is negative");
+        }
+    }
+
     @Override
     public void execute(HttpRequest request, OutputStream output) throws IOException {
         Gson gson = new Gson();
         Item newItem = gson.fromJson(request.getBody(), Item.class);
+        checkItem(newItem);
         itemsRepository.addNewItem(newItem);
         String response = "" +
                 "HTTP/1.1 201 Created\r\n" +
